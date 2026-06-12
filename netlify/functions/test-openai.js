8a6f42a43dd82c89cbfe10560709560b4cfd6a7d
CazVisuals <info@cazvisuals.com>
2026-06-12 13:32:53 -0500

    Update test-openai.js

--- netlify/functions/test-openai.js
+++ netlify/functions/test-openai.js
@@ -23,12 +23,4 @@ exports.handler = async () => {
       })
     };
   }
-};exports.handler = async () => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      keyExists: !!process.env.OPENAI_API_KEY,
-      keyLength: process.env.OPENAI_API_KEY?.length || 0
-    })
-  };
 };
